85201de5e21b506ae656098f1d0cc35d1fae6360
Domen Oslaj <doslaj@gmail.com>
2020-12-23 21:47:49 +0100

    save user into database

--- main.py
+++ main.py
@@ -1,18 +1,39 @@
-from flask import Flask, render_template, make_response, request
 import random
+from flask import Flask, render_template, request, make_response, redirect, url_for
+from models import User, db
 
 app = Flask(__name__)
+db.create_all()
 
 
 @app.route("/", methods=["GET"])
 def index():
-    secret_num = request.cookies.get("secret_num")    # check if secret num already exists in a cookie
+    email_address = request.cookies.get("email")
 
-    response = make_response(render_template("index.html"))
+    if email_address:
+        user = db.query(User).filter_by(email=email_address).first()
+    else:
+        user = None
 
-    if not secret_num:
-        new_secret = str(random.randint(0, 29))
-        response.set_cookie("secret_num", new_secret)
+    return render_template("index.html", user=user)
+
+
+@app.route("/login", methods=["POST"])
+def login():
+    name = request.form.get("user-name")
+    email = request.form.get("user-email")
+    secret_num = random.randint(1, 30)
+
+    user = db.query(User).filter_by(email=email).first()
+
+    if not user:
+        user = User(name=name, email=email, secret_num=secret_num)
+
+        db.add(user)
+        db.commit()
+
+    response = make_response(redirect(url_for("index")))
+    response.set_cookie("email", email)
 
     return response
 
@@ -20,25 +41,31 @@ def index():
 @app.route("/result", methods=["POST"])
 def result():
     guess = int(request.form.get("guess"))
-    secret_num = int(request.cookies.get("secret_num"))
+    email_address = request.cookies.get("email")
+
+    user = db.query(User).filter_by(email=email_address).first()
 
     if guess > 30 or guess < 1:
         message = "Just numbers between 1 and 30!"
-        return render_template("result.html", message=message)
 
-    elif guess == secret_num:
-        message = "Correct! Secret number is {0}!" .format(str(secret_num))
-        response = make_response(render_template("result.html", message=message))
-        response.set_cookie("secret_num", str(random.randint(0, 29)))
-        return response
+    elif guess == user.secret_num:
+        message = "Correct! Secret number is {0}!" .format(str(guess))
 
-    elif guess > secret_num:
-        message = "Not correct, try something smaller!"
-        return render_template("result.html", message=message)
+        new_secret = random.randint(1, 30)
 
-    elif guess < secret_num:
-        message = "Not correct, try something bigger!"
-        return render_template("result.html", message=message)
+        user.secret_num = new_secret
+
+        db.add(user)
+        db.commit()
+
+    elif guess > user.secret_num:
+
+        message = "Your guess is not correct... try something smaller."
+
+    elif guess < user.secret_num:
+        message = "Your guess is not correct... try something bigger."
+
+    return render_template("result.html", message=message)
 
 
 if __name__ == '__main__':
